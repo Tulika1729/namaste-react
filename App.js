cb7a49a5c19b912e72c250d66de0e60da475255a
tulika <tulika@naukri.com>
2023-09-20 21:24:09 +0530

    Inception:React basics

---
+++ App.js
@@ -0,0 +1,45 @@
+{/* <div id = "parent">
+    <div id = "child">
+        <h1>I'm h1 tag of child1</h1>
+        <h2>I'm h2 tag of child1</h2>
+    </div>
+    <div id = "child2">
+        <h1>I'm h1 tag of child2</h1>
+        <h2>I'm h2 tag of child2</h2>
+    </div>
+</div> */}
+
+
+const parent = React.createElement(
+"div", 
+{id: "parent"},[
+    React.createElement("div", {id: "child"},
+[
+    React.createElement("h1", {}, "I'm h1 tag of child1"),
+    React.createElement("h2", {}, "I'm h2 tag of child1")
+]
+),
+React.createElement("div", {id: "child2"},
+[
+    React.createElement("h1", {}, "I'm h1 tag of child1"),
+    React.createElement("h2", {}, "I'm h2 tag of child1")
+]
+),
+]
+)
+
+// it is creating h1 inside React
+//React.createElement creates an object . It is not creating h1 tag
+const heading = React.createElement(
+"h1", 
+{id: "heading"}, 
+"Hello World from React");
+
+//now creating a root which is the job of React DOM
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//this render method is responsible for taking the heading object and convet it into thr heading tag
+// and then put it in the DOM
+
+// ReactElement(Object) => HTML(Browser Understands )
+root.render(parent)
